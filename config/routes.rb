f9c9aadd51d63308ffb19807fbe427e18c4a196d
CampAsAChamp <nickschneider101@gmail.com>
2021-11-21 11:13:18 -0800

    Added About Us and Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   # Special Case for the Root/Home Page
   root 'home#index'
+
+  get 'home/about'
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
