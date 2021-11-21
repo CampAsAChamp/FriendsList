578a0072818c92c9a65cede40e5ef7e7b8eddb1b
CampAsAChamp <nickschneider101@gmail.com>
2021-11-20 20:48:38 -0800

    Updated HTML for home page and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
+
+  # Special Case for the Root/Home Page
+  root 'home#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
